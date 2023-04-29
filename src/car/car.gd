29b2614633d0dc933f8b8b28d430aa9f5a4c62ed
grace <graceschorno@protonmail.com>
2023-04-29 15:19:35 -0700

    Add asset to car; change car control

--- src/car/car.gd
+++ src/car/car.gd
@@ -1,7 +1,25 @@
 class_name Car extends RigidBody2D
 
-@export var speed = 40
+@export var speed = 1.0
+@export var steering = 1.0
+
+func _ready():
+	$AnimatedSprite2D.play("default")
 
 func _physics_process(delta):
 	var input_direction: Vector2 = Input.get_vector("Left", "Right", "Forward", "Backward")
-	apply_central_force(input_direction * speed)
+	var torque = input_direction.x
+	var f = input_direction.y
+	var rotation_direction = Vector2(0.0, 1.0).rotated(rotation)
+	if torque != 0.0:
+		apply_torque(torque * 5000000.0 * steering)
+		
+	if f != 0.0:
+		apply_central_force(speed * 800000 * rotation_direction * f)
+	
+
+	
+	
+func _process(delta):
+	
+	$AnimatedSprite2D.speed_scale = linear_velocity.length()
