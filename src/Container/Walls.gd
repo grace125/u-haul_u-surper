111d399e0a330a0161cf9cb0acdbf5053a652383
Cole Lutz <colelutz@uvic.ca>
2023-04-30 10:00:15 -0700

    added new collision function to Container scene

--- src/Container/Walls.gd
+++ src/Container/Walls.gd
@@ -1,6 +1,5 @@
 extends StaticBody2D
 
-
 func turning(direction: int, velocity: float):
 	match direction:
 		-1:
@@ -10,12 +9,21 @@ func turning(direction: int, velocity: float):
 		1:
 			constant_linear_velocity.x = velocity
 
+var collision_time = 0.0
+
+func collision(force: Vector2):
+	constant_linear_velocity.x = force.x
+	constant_linear_velocity.y = -abs(force.y)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	collision_time += delta
+	if collision_time > 1:
+		collision_time = 0.0
+		collision(Vector2(0,0))
 	
