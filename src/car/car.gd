dfac0df1fb5d4cbdd0bd34da16905ece32f41261
grace <graceschorno@protonmail.com>
2023-04-30 15:27:28 -0700

    Change how the steering signal works

--- src/car/car.gd
+++ src/car/car.gd
@@ -7,13 +7,14 @@ var last_steer_control: float = 0.0;
 var last_contact_count: float = 0.0;
 var v0: Vector2 = linear_velocity;
 var linear_acceleration: Vector2 = Vector2.ZERO;
+var r0: float = angular_velocity;
 
 signal shake(Vector2);
 
 # Emits on change of turning
 # control: -1 if left, 0 of nothing, and 1 if right
 # steer_speed: the steer speed modifier (the variable exported above)
-signal turning(control: int, steer_speed: float);
+signal turning(velocity: float);
 
 func _ready():
 	$AnimatedSprite2D.play("default")
@@ -29,16 +30,20 @@ func _physics_process(delta):
 	if forward_control != 0.0:
 		apply_central_force(speed * 800000.0 * rotation_direction * forward_control)
 		
-	if _normalize(steer_control) != last_steer_control:
-		last_steer_control = _normalize(steer_control)
-		turning.emit(last_steer_control, steer_speed*50.0)
+	# if _normalize(steer_control) != last_steer_control:
+	# 	last_steer_control = _normalize(steer_control)
+	# 	turning.emit(last_steer_control, steer_speed*50.0)
 	
 func _process(delta):
 	var rotation_direction = Vector2(0.0, 1.0).rotated(rotation)
 	$AnimatedSprite2D.speed_scale = linear_velocity.dot(rotation_direction)
 	
 	linear_acceleration = (linear_velocity - v0) / delta
-	v0 = linear_velocity
+	
+	if angular_velocity != r0:
+		turning.emit(r0)
+		r0 = angular_velocity
+	
 
 func _normalize(x: float):
 	if x < 0.0:
