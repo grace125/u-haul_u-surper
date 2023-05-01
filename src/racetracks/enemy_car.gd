181a8b2fe5c12bd487cb784493029913d317199e
Medow3 <matthew.dawsonhome@gmail.com>
2023-04-30 17:32:28 -0700

    Add second AI

--- src/racetracks/enemy_car.gd
+++ src/racetracks/enemy_car.gd
@@ -1,12 +1,17 @@
 class_name EnemyCar extends RigidBody2D
 
 @export var speed: float = 30.0
-@export var max_speed: float = 100.0
+@export var max_speed: float = 110.0
 @export var waypoint_distance_threshold: float = 40.0
+@export var car_color: Color
 
+@onready var sprite = $Sprite2D
 
 var waypoints_list: Array #Set by the enemy_cars node
 var current_waypoint_index: int = 0
+
+func _ready():
+	sprite.modulate = car_color
 
 func _physics_process(delta):
 	if global_position.distance_to(waypoints_list[current_waypoint_index]) <= waypoint_distance_threshold:
@@ -16,4 +21,6 @@ func _physics_process(delta):
 		print("current_waypoint_index ", current_waypoint_index, " ", waypoints_list[current_waypoint_index])
 	if linear_velocity.length() < max_speed:
 		apply_central_force(800000.0 * global_position.direction_to(waypoints_list[current_waypoint_index]) * speed * delta)
+	if linear_velocity != Vector2.ZERO:
+		rotation = Vector2(0.0, -1.0).angle_to(linear_velocity)
 	
