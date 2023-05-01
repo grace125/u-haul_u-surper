f708fe020920da1034a0fb13e0ef2a997e566913
grace <graceschorno@protonmail.com>
2023-05-01 16:07:21 -0700

    Add new song

--- src/main_menu/main_menu.gd
+++ src/main_menu/main_menu.gd
@@ -1,9 +1,8 @@
 extends Control
 
 func _ready():
-	Music.play_song("ThemeSong_1", Music.DEFAULT_MUSIC_VOLUME, 0.2)
-	
-
+	Music.play_song("Title_Theme_2", Music.DEFAULT_MUSIC_VOLUME, 0.2)
 
 func _on_button_pressed():
+	Music.play_song("ThemeSong_1", Music.DEFAULT_MUSIC_VOLUME, 0.2)
 	get_tree().change_scene_to_file("res://src/game/main_race.tscn")
