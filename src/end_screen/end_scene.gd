4895f95df94265b39d72fae84f970108e5bf05d4
Cole Lutz <colelutz@uvic.ca>
2023-05-01 15:54:18 -0700

    Fixed crash on game completion

--- src/end_screen/end_scene.gd
+++ src/end_screen/end_scene.gd
@@ -3,6 +3,7 @@ extends Control
 var score: int = 0
 var place: int = 0
 var boxes_left: int = 0
+var total_boxes: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
